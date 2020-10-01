7ee5490fdb9da36da52293a0048f693e0ec95680
Kaykobad Reza <kaykobad.reza@bkash.com>
2020-10-01 08:47:09 +0600

    Add custom user model and add signals to handle it appropriately

--- account/models.py
+++ account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    mobile_number = models.CharField(max_length=15, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=30, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_picture/', default='default_profile_pic.png')
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
